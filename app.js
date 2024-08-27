4f14030daf67b7dce3eb889fc5bad42d4798756e
parv141206 <parv141206@gmail.com>
2024-08-27 08:05:45 +0530

    Added some teams and increased limits

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const cors = require("cors");
 const app = express();
 
 const laLigaTeams = [
-  { name: "Atlético Madrid", aliases: ["Atleti"], league: "LaLiga" },
+  {
+    name: "Atlético Madrid",
+    aliases: ["Atleti", "Atlético", "Atleti Madrid"],
+    league: "LaLiga",
+  },
   { name: "Real Madrid", aliases: ["Los Blancos", "Madrid"], league: "LaLiga" },
+  { name: "Las Palmas", aliases: ["Las Palmas"], league: "LaLiga" },
   { name: "Barcelona", aliases: ["Barça"], league: "LaLiga" },
+  { name: "A Bilbao", aliases: ["El Bilbó"], league: "LaLiga" },
   { name: "Sevilla", aliases: ["Los Nervionenses"], league: "LaLiga" },
   { name: "Real Betis", aliases: ["Los Verdiblancos"], league: "LaLiga" },
   { name: "Real Sociedad", aliases: ["La Real"], league: "LaLiga" },
+  { name: "Alves", aliases: ["Los Negros"], league: "LaLiga" },
   { name: "Villarreal", aliases: ["El Submarino Amarillo"], league: "LaLiga" },
   { name: "Athletic Bilbao", aliases: ["Los Leones"], league: "LaLiga" },
   { name: "Osasuna", aliases: ["Los Rojillos"], league: "LaLiga" },
@@ -25,7 +32,7 @@ const laLigaTeams = [
   { name: "Girona", aliases: ["Los Blanquivermells"], league: "LaLiga" },
   { name: "Mallorca", aliases: ["Los Bermellones"], league: "LaLiga" },
   { name: "Elche", aliases: ["Los Franjiverdes"], league: "LaLiga" },
-  { name: "Levante", aliases: ["Los Granotas"], league: "LaLiga" }
+  { name: "Levante", aliases: ["Los Granotas"], league: "LaLiga" },
 ];
 
 const premierLeagueTeams = [
@@ -35,20 +42,45 @@ const premierLeagueTeams = [
   { name: "Brentford", aliases: ["The Bees"], league: "Premier League" },
   { name: "Brighton", aliases: ["The Seagulls"], league: "Premier League" },
   { name: "Chelsea", aliases: ["The Blues"], league: "Premier League" },
-  { name: "Crystal Palace", aliases: ["The Eagles"], league: "Premier League" },
+  {
+    name: "Crystal Palace",
+    aliases: ["The Eagles", "C Palace"],
+    league: "Premier League",
+  },
   { name: "Everton", aliases: ["The Toffees"], league: "Premier League" },
   { name: "Fulham", aliases: ["The Cottagers"], league: "Premier League" },
+  { name: "Ipswich", aliases: ["The Foxes"], league: "Premier League" },
   { name: "Leeds United", aliases: ["The Whites"], league: "Premier League" },
   { name: "Leicester City", aliases: ["The Foxes"], league: "Premier League" },
   { name: "Liverpool", aliases: ["The Reds"], league: "Premier League" },
   { name: "Manchester City", aliases: ["Man City"], league: "Premier League" },
-  { name: "Manchester United", aliases: ["Man Utd", "The Red Devils"], league: "Premier League" },
-  { name: "Newcastle United", aliases: ["The Magpies"], league: "Premier League" },
-  { name: "Nottingham Forest", aliases: ["The Reds"], league: "Premier League" },
+  {
+    name: "Manchester United",
+    aliases: ["Man Utd", "The Red Devils"],
+    league: "Premier League",
+  },
+  {
+    name: "Newcastle United",
+    aliases: ["The Magpies", "Newcastle"],
+    league: "Premier League",
+  },
+  {
+    name: "Nottingham Forest",
+    aliases: ["The Reds", "Nottm Forest"],
+    league: "Premier League",
+  },
   { name: "Southampton", aliases: ["The Saints"], league: "Premier League" },
   { name: "Tottenham Hotspur", aliases: ["Spurs"], league: "Premier League" },
-  { name: "West Ham United", aliases: ["The Hammers"], league: "Premier League" },
-  { name: "Wolverhampton Wanderers", aliases: ["Wolves"], league: "Premier League" }
+  {
+    name: "West Ham United",
+    aliases: ["The Hammers", "West Ham"],
+    league: "Premier League",
+  },
+  {
+    name: "Wolverhampton Wanderers",
+    aliases: ["Wolves"],
+    league: "Premier League",
+  },
 ];
 
 const PORT = process.env.PORT || 8080;
@@ -62,13 +94,14 @@ app.get("/", async (req, res) => {
     let url = "https://www.theguardian.com/football/fixtures";
     let clickCount = 0;
 
-    while (url && clickCount < 5) {
+    while (url && clickCount < 10) {
       console.log("Fetching URL:", url);
       const { data } = await axios.get(url, {
         headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
         },
-        timeout: 10000 // Set a timeout for the request
+        timeout: 10000, // Set a timeout for the request
       });
       const $ = cheerio.load(data);
       const fixtures = extractFixtures($);
@@ -76,7 +109,10 @@ app.get("/", async (req, res) => {
 
       const moreButton = $(".football-matches__show-more");
       if (moreButton.length) {
-        url = new URL(moreButton.attr("href"), "https://www.theguardian.com").toString();
+        url = new URL(
+          moreButton.attr("href"),
+          "https://www.theguardian.com"
+        ).toString();
         clickCount++;
       } else {
         url = null;
@@ -98,8 +134,16 @@ function extractFixtures($) {
     const matches = $(element).find(".football-match--fixture");
 
     matches.each((matchIndex, matchElement) => {
-      const time = $(matchElement).find(".football-match__status").text().trim().replace(/\s+/g, " ");
-      const teams = $(matchElement).find(".football-match__teams").text().trim().replace(/\s+/g, " ");
+      const time = $(matchElement)
+        .find(".football-match__status")
+        .text()
+        .trim()
+        .replace(/\s+/g, " ");
+      const teams = $(matchElement)
+        .find(".football-match__teams")
+        .text()
+        .trim()
+        .replace(/\s+/g, " ");
       const formattedMatch = formatMatch(teams);
       if (formattedMatch) {
         const res = { date, time, match: formattedMatch };
@@ -121,7 +165,7 @@ function formatMatch(teams) {
         return {
           match: `${team1} vs ${team2}`,
           league1,
-          league2
+          league2,
         };
       }
     }
